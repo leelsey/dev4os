2a461dd95ce1199bf8ee63e49ee65c41c7713df8
Leslie Elsey <108100202+leelsey@users.noreply.github.com>
2022-07-31 13:28:12 +0900

    Update dev4win.go code like dev4mac.go

--- cmd/dev4win/dev4win.go
+++ cmd/dev4win/dev4win.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/briandowns/spinner"
-	"github.com/schollz/progressbar/v3"
 	"golang.org/x/sys/windows"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -16,18 +16,16 @@ import (
 )
 
 var (
-	appVer   = "0.1"
-	Git4setV = "Git4set-0.1"
-	lstDot   = " • "
-	dlPath   = homeDir() + "Downloads\\"
-	pSh      = "powershell"
-	cmdPMS   = "C:\\ProgramData\\chocolatey\\choco.exe"
-	cmdIn    = "install"
-	cmdRein  = "reinstall"
-	cmdRm    = "uninstall"
-	cmdY     = "-y"
-	cmdGit   = "C:\\'Program Files'\\git\\bin\\git.exe"
-	cmdOpt   string
+	appVer = "0.1"
+	lstDot = " • "
+	pSh    = "powershell"
+	cmdPMS = "C:\\ProgramData\\chocolatey\\choco.exe"
+	cmdIn  = "install"
+	//cmdRein  = "reinstall"
+	//cmdRm    = "uninstall"
+	cmdY   = "-y"
+	cmdGit = "C:\\'Program Files'\\git\\bin\\git.exe"
+	cmdOpt string
 )
 
 func checkError(err error) bool {
@@ -35,6 +33,18 @@ func checkError(err error) bool {
 		fmt.Println(err.Error())
 	}
 	return err != nil
+}
+
+func checkNetStatus() bool {
+	getTimeout := 10000 * time.Millisecond
+	client := http.Client{
+		Timeout: getTimeout,
+	}
+	_, err := client.Get("https://9.9.9.9")
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func homeDir() string {
@@ -57,8 +67,8 @@ func restartWin() {
 	fmt.Println("Restarting now ...")
 	if err := exec.Command(pSh, "shutdown", "/r", "/t", "0").Run(); err != nil {
 		fmt.Println(" - Failed to restart Windows")
-		winEnd()
 	}
+	os.Exit(0)
 }
 
 func runElevated() {
@@ -76,36 +86,90 @@ func runElevated() {
 }
 
 func confG4s() {
-	dlG4s := dlPath + Git4setV + ".zip"
-	req, _ := http.NewRequest("GET", "https://github.com/leelsey/Git4set/archive/refs/tags/v0.1.zip", nil)
-	resp, _ := http.DefaultClient.Do(req)
-	defer resp.Body.Close()
-	file, _ := os.OpenFile(dlG4s, os.O_CREATE|os.O_WRONLY, 0755)
-	defer file.Close()
-	bar := progressbar.DefaultBytes(
-		resp.ContentLength,
-		"Downloading Git4set...",
-	)
-	io.Copy(io.MultiWriter(file, bar), resp.Body)
+	fmt.Println("\nGit global configuration")
 
-	fmt.Println(" - Finished to download Git4sh: " + dlG4s + " (Your download directory)\n" +
-		"\nPlease extract zip file and run script on terminal.\n" +
-		lstDot + "Configure global author & ignore: initial-git\n" +
-		lstDot + "Only want configure global author: git-conf\n" +
-		lstDot + "Only want configure global ignore: git-ignore")
+	fmt.Println(" 1) Main branch default name changed master -> main")
+	setBranchMain := exec.Command("git", "config", "--global", "init.defaultBranch", "main")
+	if err := setBranchMain.Run(); err != nil {
+		checkError(err)
+	}
+
+	fmt.Println(" 2) Add your information to the global git config")
+	consoleReader := bufio.NewScanner(os.Stdin)
+	fmt.Printf(" " + lstDot + "User name: ")
+	consoleReader.Scan()
+	userName := consoleReader.Text()
+	fmt.Printf(" " + lstDot + "User email: ")
+	consoleReader.Scan()
+	userEmail := consoleReader.Text()
+
+	setUserName := exec.Command("git", "config", "--global", "user.name", userName)
+	setUserEmail := exec.Command("git", "config", "--global", "user.email", userEmail)
+	if err := setUserName.Run(); err != nil {
+		checkError(err)
+	}
+	if err := setUserEmail.Run(); err != nil {
+		checkError(err)
+	}
+
+	fmt.Println(" 3) Setup git global ignore file with directories")
+	ignoreDir := homeDir() + ".config\\git\\"
+	if err := os.MkdirAll(ignoreDir, 0755); err != nil {
+		checkError(err)
+	}
+
+	ignorePath := ignoreDir + "gitignore_global"
+	resp, err := http.Get("https://raw.githubusercontent.com/leelsey/Git4set/main/gitignore-sample")
+	if err != nil {
+		fmt.Println(lstDot + "Git Ignore sample URL is maybe changed, please check https://github.com/leelsey/Git4set\n")
+		os.Exit(0)
+	}
+	defer func() {
+		err := resp.Body.Close()
+		checkError(err)
+	}()
+	rawFile, _ := ioutil.ReadAll(resp.Body)
+
+	gitIgnore, err := os.OpenFile(ignorePath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.FileMode(0600))
+	checkError(err)
+	defer func() {
+		err := gitIgnore.Close()
+		checkError(err)
+	}()
+	_, err = gitIgnore.Write(rawFile)
+	checkError(err)
+
+	setExcludesFile := exec.Command("git", "config", "--global", "core.excludesfile", ignorePath)
+	if err := setExcludesFile.Run(); err != nil {
+		fmt.Println("error2")
+		checkError(err)
+	}
+
+	fmt.Println(" " + lstDot + "Make \"gitignore_global\" file in " + ignoreDir)
 }
 
-func winChoco() {
+func updateChoco() {
+	updateChocolatey := exec.Command(cmdPMS, "upgrade", cmdY, "all")
+	if err := updateChocolatey.Run(); err != nil {
+		checkError(err)
+	}
+}
+
+func installChoco() {
+	installChocolatey := exec.Command(pSh, `Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))`)
+	if err := installChocolatey.Run(); err != nil {
+		checkError(err)
+	}
+}
+
+func winBegin() {
 	if _, err := os.Stat("C:\\ProgramData\\Chocolatey"); !os.IsNotExist(err) {
 		ldBar := spinner.New(spinner.CharSets[43], 500*time.Millisecond)
 		ldBar.Suffix = " Updating chocolatey..."
 		ldBar.FinalMSG = " - Updated choco!\n"
 		ldBar.Start()
 
-		updateChocolatey := exec.Command(cmdPMS, "upgrade", cmdY, "all")
-		updatingHomebrew, err := updateChocolatey.Output()
-		checkError(err)
-		fmt.Sprintf(string(updatingHomebrew))
+		updateChoco()
 		ldBar.Stop()
 	} else {
 		ldBar := spinner.New(spinner.CharSets[43], 500*time.Millisecond)
@@ -113,10 +177,8 @@ func winChoco() {
 		ldBar.FinalMSG = " - Installed choco!\n"
 		ldBar.Start()
 
-		installChocolatey := exec.Command(pSh, `Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))`)
-		installingChocolatey, err := installChocolatey.Output()
-		checkError(err)
-		fmt.Sprintf(string(installingChocolatey))
+		installChoco()
+		updateChoco()
 		ldBar.Stop()
 	}
 }
@@ -129,22 +191,13 @@ func winGit() {
 
 	installGit := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "git")
 	installGitLfs := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "git-lfs")
-	gitLfsInstall := exec.Command(pSh, cmdGit, "lfs", "install")
-	gitBranchMain := exec.Command(pSh, cmdGit, "config", "--global", "init.defaultBranch", "main")
 
-	installingGit, err := installGit.Output()
-	checkError(err)
-	installingGitLfs, err := installGitLfs.Output()
-	checkError(err)
-	gitLfsInstalling, err := gitLfsInstall.Output()
-	checkError(err)
-	confGitBranchMain, err := gitBranchMain.Output()
-	checkError(err)
-
-	fmt.Sprintf(string(installingGit))
-	fmt.Sprintf(string(installingGitLfs))
-	fmt.Sprintf(string(gitLfsInstalling))
-	fmt.Sprintf(string(confGitBranchMain))
+	if err := installGit.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installGitLfs.Run(); err != nil {
+		checkError(err)
+	}
 	ldBar.Stop()
 }
 
@@ -164,34 +217,33 @@ func winDependency() {
 	installRe2C := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "re2c")
 	installGhostscript := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "ghostscript")
 
-	installingSSL, err := installSSL.Output()
-	checkError(err)
-	installingGnuPG, err := installGnuPG.Output()
-	checkError(err)
-	installingcURL, err := installcURL.Output()
-	checkError(err)
-	installingWget, err := installWget.Output()
-	checkError(err)
-	installingGzip, err := installGzip.Output()
-	checkError(err)
-	installingBzip2, err := installBzip2.Output()
-	checkError(err)
-	installingCoreUtils, err := installCoreUtils.Output()
-	checkError(err)
-	installingRe2C, err := installRe2C.Output()
-	checkError(err)
-	installingGhostscript, err := installGhostscript.Output()
-	checkError(err)
-
-	fmt.Sprintf(string(installingSSL))
-	fmt.Sprintf(string(installingGnuPG))
-	fmt.Sprintf(string(installingcURL))
-	fmt.Sprintf(string(installingWget))
-	fmt.Sprintf(string(installingGzip))
-	fmt.Sprintf(string(installingBzip2))
-	fmt.Sprintf(string(installingCoreUtils))
-	fmt.Sprintf(string(installingRe2C))
-	fmt.Sprintf(string(installingGhostscript))
+	if err := installSSL.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installGnuPG.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installcURL.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installWget.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installGzip.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installBzip2.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installCoreUtils.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installRe2C.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installGhostscript.Run(); err != nil {
+		checkError(err)
+	}
 	ldBar.Stop()
 }
 
@@ -214,43 +266,42 @@ func winDevToolCLI() {
 	installCygwin := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "cygwin")
 	installVS := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "visualstudio2022community")
 
-	installingGawk, err := installGawk.Output()
-	checkError(err)
-	installingJQ, err := installJQ.Output()
-	checkError(err)
-	installingWatchman, err := installWatchman.Output()
-	checkError(err)
-	installingQEMU, err := installQEMU.Output()
-	checkError(err)
-	installingCcache, err := installCcache.Output()
-	checkError(err)
-	installingMake, err := installMake.Output()
-	checkError(err)
-	installingVim, err := installVim.Output()
-	checkError(err)
-	installingBat, err := installBat.Output()
-	checkError(err)
-	installingGH, err := installGH.Output()
-	checkError(err)
-	installingPS, err := installPS.Output()
-	checkError(err)
-	installingCygwin, err := installCygwin.Output()
-	checkError(err)
-	installingVS, err := installVS.Output()
-	checkError(err)
-
-	fmt.Sprintf(string(installingGawk))
-	fmt.Sprintf(string(installingJQ))
-	fmt.Sprintf(string(installingWatchman))
-	fmt.Sprintf(string(installingQEMU))
-	fmt.Sprintf(string(installingCcache))
-	fmt.Sprintf(string(installingMake))
-	fmt.Sprintf(string(installingVim))
-	fmt.Sprintf(string(installingBat))
-	fmt.Sprintf(string(installingGH))
-	fmt.Sprintf(string(installingPS))
-	fmt.Sprintf(string(installingCygwin))
-	fmt.Sprintf(string(installingVS))
+	if err := installGawk.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installJQ.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installWatchman.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installQEMU.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installCcache.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installMake.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installVim.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installBat.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installGH.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installPS.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installCygwin.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installVS.Run(); err != nil {
+		checkError(err)
+	}
 	ldBar.Stop()
 }
 
@@ -266,22 +317,21 @@ func winServer() {
 	installPostgreSQL := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "postgresql")
 	installMySQL := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "mysql")
 
-	installingHTTPD, err := installHTTPD.Output()
-	checkError(err)
-	installingTomcat, err := installTomcat.Output()
-	checkError(err)
-	installingSQLite, err := installSQLite.Output()
-	checkError(err)
-	installingPostgreSQL, err := installPostgreSQL.Output()
-	checkError(err)
-	installingMySQL, err := installMySQL.Output()
-	checkError(err)
-
-	fmt.Sprintf(string(installingHTTPD))
-	fmt.Sprintf(string(installingTomcat))
-	fmt.Sprintf(string(installingSQLite))
-	fmt.Sprintf(string(installingPostgreSQL))
-	fmt.Sprintf(string(installingMySQL))
+	if err := installHTTPD.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installTomcat.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installSQLite.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installPostgreSQL.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installMySQL.Run(); err != nil {
+		checkError(err)
+	}
 	ldBar.Stop()
 }
 
@@ -309,58 +359,57 @@ func winLanguage() {
 	installErlang := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "erlang")
 	installElixir := exec.Command(pSh, cmdPMS, cmdIn, cmdY, "elixir")
 
-	installingGCC, err := installGCC.Output()
-	checkError(err)
-	installingLLVM, err := installLLVM.Output()
-	checkError(err)
-	installingNuGet, err := installNuGet.Output()
-	checkError(err)
-	installingPerl, err := installPerl.Output()
-	checkError(err)
-	installingRuby, err := installRuby.Output()
-	checkError(err)
-	installingPython, err := installPython.Output()
-	checkError(err)
-	installingLua, err := installLua.Output()
-	checkError(err)
-	installingGo, err := installGo.Output()
-	checkError(err)
-	installingRust, err := installRust.Output()
-	checkError(err)
-	installingNode, err := installNode.Output()
-	checkError(err)
-	installingPHP, err := installPHP.Output()
-	checkError(err)
-	installingJDK, err := installJDK.Output()
-	checkError(err)
-	installingGroovy, err := installGroovy.Output()
-	checkError(err)
-	installingScala, err := installScala.Output()
-	checkError(err)
-	installingClojure, err := installClojure.Output()
-	checkError(err)
-	installingErlang, err := installErlang.Output()
-	checkError(err)
-	installingElixir, err := installElixir.Output()
-	checkError(err)
-
-	fmt.Sprintf(string(installingGCC))
-	fmt.Sprintf(string(installingLLVM))
-	fmt.Sprintf(string(installingNuGet))
-	fmt.Sprintf(string(installingPerl))
-	fmt.Sprintf(string(installingRuby))
-	fmt.Sprintf(string(installingPython))
-	fmt.Sprintf(string(installingLua))
-	fmt.Sprintf(string(installingGo))
-	fmt.Sprintf(string(installingRust))
-	fmt.Sprintf(string(installingNode))
-	fmt.Sprintf(string(installingPHP))
-	fmt.Sprintf(string(installingJDK))
-	fmt.Sprintf(string(installingGroovy))
-	fmt.Sprintf(string(installingScala))
-	fmt.Sprintf(string(installingClojure))
-	fmt.Sprintf(string(installingErlang))
-	fmt.Sprintf(string(installingElixir))
+	if err := installGCC.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installLLVM.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installNuGet.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installPerl.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installRuby.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installPython.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installLua.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installGo.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installRust.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installNode.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installPHP.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installJDK.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installGroovy.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installScala.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installClojure.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installErlang.Run(); err != nil {
+		checkError(err)
+	}
+	if err := installElixir.Run(); err != nil {
+		checkError(err)
+	}
 	ldBar.Stop()
 }
 
@@ -371,19 +420,10 @@ func winWLS() {
 	ldBar.Start()
 
 	setWSL := exec.Command(pSh, "wsl", "--install")
-	settingWSL, err := setWSL.Output()
-	checkError(err)
-	fmt.Sprintf(string(settingWSL))
+	if err := setWSL.Run(); err != nil {
+		checkError(err)
+	}
 	ldBar.Stop()
-}
-
-func winEnd() {
-	fmt.Println("\n----------Finished!----------\n" +
-		"Please RESTART your terminal and OS!\n" +
-		lstDot + "Restart the terminal (CMD or PowerShell) for the changes to take effect.\n" +
-		lstDot + "WSL has been setup. Restart OS for the changes to take effect.\n" +
-		"\nPress 'Enter' to exit...")
-	fmt.Scanln()
 }
 
 func main() {
@@ -392,38 +432,48 @@ func main() {
 	}
 	if checkAdmin() {
 		fmt.Println("\nDev4win v" + appVer + "\n")
-		winChoco()
-		winGit()
-		winDependency()
-		winDevToolCLI()
-		winServer()
-		winLanguage()
-		winWLS()
-		fmt.Println("\nFinished to setup! You can choose 4 options. (Recommend option is 1)\n" +
-			"\t1. Restart OS after download Git4set\n" +
-			"\t2. Restart Windows operating system\n" +
-			"\t3. Download easily configure global git (Git4set)\n" +
-			"\t0. Nothing, finish Dev4win")
-		for {
-			fmt.Printf("\nSelect command: ")
-			fmt.Scanln(&cmdOpt)
-			if cmdOpt == "1" {
-				confG4s()
-				restartWin()
+		if checkNetStatus() == true {
+			winBegin()
+			winGit()
+			winDependency()
+			winDevToolCLI()
+			winServer()
+			winLanguage()
+			winWLS()
+			fmt.Println("\nFinished to setup! You can choose 4 options. (Recommend option is 1)\n" +
+				"\t1. Restart OS after download Git4set\n" +
+				"\t2. Restart Windows operating system\n" +
+				"\t3. Download easily configure global git (Git4set)\n" +
+				"\t0. Nothing, finish Dev4win")
+		endOpt:
+			for {
+				fmt.Printf("\nSelect command: ")
+				_, err := fmt.Scanln(&cmdOpt)
+				checkError(err)
+				if cmdOpt == "1" {
+					confG4s()
+					restartWin()
+				} else if cmdOpt == "2" {
+					restartWin()
+				} else if cmdOpt == "3" {
+					confG4s()
+				} else if cmdOpt == "0" || cmdOpt == "q" || cmdOpt == "e" || cmdOpt == "quit" || cmdOpt == "exit" {
+				} else {
+					fmt.Println("Wrong answer. Please choose between 1,2,3,0.")
+					goto endOpt
+				}
 				break
-			} else if cmdOpt == "2" {
-				restartWin()
-				break
-			} else if cmdOpt == "3" {
-				confG4s()
-				winEnd()
-				break
-			} else if cmdOpt == "0" {
-				winEnd()
-				break
-			} else {
-				fmt.Println("Wrong answer. Please choose between 1,2,3,0.")
 			}
+			fmt.Println("\n----------Finished!----------\n" +
+				"Please RESTART your terminal and OS!\n" +
+				lstDot + "Restart the terminal (CMD or PowerShell) for the changes to take effect.\n" +
+				lstDot + "WSL has been setup. Restart OS for the changes to take effect.\n" +
+				"\nPress 'Enter' to exit...")
+			_, err := fmt.Scanln()
+			checkError(err)
+		} else {
+			fmt.Println(lstDot + "Please check your internet connection and try again.\n")
 		}
+
 	}
 }
